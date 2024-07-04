29516623479e878e253b7ce4e0fdcfc3f92fba03
Ent Thura <entthura@gmail.com>
2024-07-05 00:41:37 +0700

    Full pipeline test

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello Dear My Love!'));
+app.get('/', (req, res) => res.send('Dear My Love of My Life!'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
